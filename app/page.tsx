2a78281c8bfa8f185f98ab9618b986893804cd96
Phongsavanh Mongkhonvilay <pmongkho12@gmail.com>
2023-09-10 11:56:01 -0500

    login skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,10 @@ import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
 
 export default async function Home() {
-  const session = await getServerSession()
   
-  if (!session) {
+
     redirect('/sign-in')  
-  }
+  
 		return (
 			<main className=' bg-slate-800 h-screen flex items-center justify-center'>
 				Home Page
