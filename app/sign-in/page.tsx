6287fb7b1dcda9cb9088ccd77f45a2d7a154a753
Phongsavanh Mongkhonvilay <pmongkho12@gmail.com>
2023-09-10 10:43:23 -0500

    login skeleton

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -8,7 +8,7 @@ export default function SignIn() {
 					href='#'
 					className='flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white'
 				>
-					ParkEZ
+					Welcome to ParkEZ
 				</a>
 				<div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
 					<div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
