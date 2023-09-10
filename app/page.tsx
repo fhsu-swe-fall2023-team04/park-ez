88970899e16bc1f1908e445d147d86ebb90aa311
Phongsavanh Mongkhonvilay <azulacorp12@gmail.com>
2023-09-09 22:22:17 -0500

    login

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-export default function Home() {
-	return (
-		<main className=' bg-slate-800 h-screen flex items-center justify-center'>
-			Home Page
-		</main>
-	)
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
+
+export default async function Home() {
+  const session = await getServerSession()
+  
+  if (!session) {
+    redirect('/sign-in')  
+  }
+		return (
+			<main className=' bg-slate-800 h-screen flex items-center justify-center'>
+				Home Page
+			</main>
+		)
 }
