cf61b2a3a55e549e363354daf6113b4761dca842
Phongsavanh Mongkhonvilay <pmongkho12@gmail.com>
2023-10-24 15:30:05 -0500

    signup post debug

--- app/api/customers/route.ts
+++ app/api/customers/route.ts
@@ -1,17 +1,36 @@
 import Customer from '@/_models/Customer';
+import Vehicle from '@/_models/Vehicle';
 import startDb from '@/_utils/startDb';
 import { NextResponse } from 'next/server';
 
 // get request
-export const GET = async (req:Request) => {
+export const GET = async (req: Request) => {
     await startDb()
     const customers = await Customer.find()
     return NextResponse.json(customers)
 }
 
-
+// post request
 export const POST = async (req: Request) => {
+    
+    await startDb()
+
     const body = await req.json()
-    const customer = await Customer.create(...body)
-    return NextResponse.json(customer)
+    const customerObj = body.customer
+    const vehicleObj = body.vehicle
+
+    // create customer
+    const customer = await Customer.create(customerObj)
+
+    // create vehicle
+    const vehicle = await Vehicle.create(vehicleObj)
+
+    // update vehicles array in customer
+    	await Customer.findOneAndUpdate(
+			{ _id: customer._id },
+			{ $push: { vehicles: vehicle._id } }
+		)
+
+    // return customer
+    return NextResponse.json({customer:customer})
 }
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -10,12 +10,16 @@ import {redirect} from 'next/navigation'
 export default function SignUp() {
 
 	const handleSubmit = async (fd: FormData) => {
+
+		'use server'
+		// customer
 		const firstName = fd.get('firstName')?.toString()
 		const lastName = fd.get('lastName')?.toString()
 		const email = fd.get('email')?.toString()
 		const phone = fd.get('phone')?.toString()
 		const paymentMethod = fd.get('payment')?.toString()
 
+		// vehicles
 		const licensePlate = fd.get('licenseplate')?.toString()
 		const make = fd.get('make')?.toString()
 		const model = fd.get('model')?.toString()
@@ -30,23 +34,22 @@ export default function SignUp() {
 			year
 		}
 
-
 		const customer = {
 			firstName,
 			lastName,
 			email,
 			phone,
-			vehicle,
 			paymentMethod
 		}
 
 		try {
-			const res = await fetch('/api/customers', {
+			const res = await fetch(`${process.env.URL}/api/customers`, {
 				method: 'POST',
-				body: JSON.stringify(customer),
-			}).then((res) => res.json())
-			// redirect to login
-			redirect('/login')
+				body: JSON.stringify({
+					customer: customer,
+					vehicle: vehicle
+				}),
+			})
 		} catch (error) {
 			throw error
 		}
@@ -69,12 +72,11 @@ export default function SignUp() {
 						</h1>
 
 						{/* form */}
-						<form className='space-y-4 md:space-y-6' action='#'>
+						<form className='space-y-4 md:space-y-6' action={handleSubmit}>
 							{/* first name */}
 							<input
 								type='text'
-								name='name'
-								id='name'
+								name='firstName'
 								className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
 								placeholder='First Name'
 								required
@@ -83,8 +85,7 @@ export default function SignUp() {
 							{/* last name */}
 							<input
 								type='text'
-								name='name'
-								id='name'
+								name='lastName'
 								className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
 								placeholder='Last Name'
 								required
@@ -95,9 +96,18 @@ export default function SignUp() {
 								<input
 									type='email'
 									name='email'
-									id='imal'
 									className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
 									placeholder='Email'
+									required
+								/>
+							</div>
+							{/* phone */}
+							<div>
+								<input
+									type='phone'
+									name='phone'
+									className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+									placeholder='Phone'
 									required
 								/>
 							</div>
