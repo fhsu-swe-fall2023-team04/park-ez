6998592166ddbc2e3acd1c5d9923a09fd47ae5ee
Phongsavanh Mongkhonvilay <pmongkho12@gmail.com>
2023-11-09 22:54:32 -0600

    parking-space

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -152,7 +152,7 @@ export default async function SignUp() {
 									<input
 										type='text'
 										name='licensePlate'
-										placeholder='License Plate'
+										placeholder='License Plate (upload pic required)'
 										className=' w-full bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
 									/>
 								</div>{' '}
