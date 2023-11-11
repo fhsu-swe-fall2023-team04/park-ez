edf09f5359c26a8a3e27a71d0eadac9d88d040e1
Phongsavanh Mongkhonvilay <pmongkho12@gmail.com>
2023-11-11 10:22:46 -0600

    fix occupy leave parking space

--- app/user-menu/wallet/page.tsx
+++ app/user-menu/wallet/page.tsx
@@ -7,70 +7,70 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 })
 
 export default async function Wallet() {
-	'use server'
+	// 'use server'
 
-	const paymentMethods = await stripe.customers.listPaymentMethods(
-		'cus_OvsQ7xyhnCsO8t',
-		{ type: 'card', limit: 1 }
-	)
+	// const paymentMethods = await stripe.customers.listPaymentMethods(
+	// 	'cus_OvtlbdO1SxVoR4',
+	// 	{ type: 'card', limit: 1 }
+	// )
 	
-	const handleTopUp = async (fd: FormData) => {
-		'use server'
+	// const handleTopUp = async (fd: FormData) => {
+	// 	'use server'
 
-		const param: Stripe.ChargeCreateParams = {
-			amount: 100,
-			currency: 'usd',
-			description: 'Test Payment',
-			customer: 'cus_OvtlbdO1SxVoR4',
-			receipt_email: 'm_mikaelian@mail.fhsu.edu',
-		}
+	// 	const param: Stripe.ChargeCreateParams = {
+	// 		amount: 100,
+	// 		currency: 'usd',
+	// 		description: 'Test Payment',
+	// 		customer: 'cus_OvtlbdO1SxVoR4',
+	// 		receipt_email: 'm_mikaelian@mail.fhsu.edu',
+	// 	}
 
-		try {
-			await stripe.charges.create(param);
+	// 	try {
+	// 		await stripe.charges.create(param);
 
-			/* Add transaction to MongoDB
-			var transaction_id = (await stripe.charges.create(param)).id;
+	// 		/* Add transaction to MongoDB
+	// 		var transaction_id = (await stripe.charges.create(param)).id;
 
-			const transaction = {
-				totalCost: 20,
-				customer: CustomerDocument['_id'],
-				space: TransactionDocument['_id'],
-				status: statusEnum
-			}
+	// 		const transaction = {
+	// 			totalCost: 20,
+	// 			customer: CustomerDocument['_id'],
+	// 			space: TransactionDocument['_id'],
+	// 			status: statusEnum
+	// 		}
 
-			await fetch(`${process.env.URL}/api/transactions`, {
-				method: 'POST',
-				body: JSON.stringify({
-					transaction: transaction
-				}),
-			})
-			*/
-		} catch (error) {
-			console.log(error);
-			throw error;
-		}
-	}
+	// 		await fetch(`${process.env.URL}/api/transactions`, {
+	// 			method: 'POST',
+	// 			body: JSON.stringify({
+	// 				transaction: transaction
+	// 			}),
+	// 		})
+	// 		*/
+	// 	} catch (error) {
+	// 		console.log(error);
+	// 		throw error;
+	// 	}
+	// }
 
-	const setUpPaymentMethod = async (fd: FormData) => {
-		'use server'
-		try {
-			//Save credit card info
-			const session = await stripe.checkout.sessions.create({
-				payment_method_types: ['card'],
-				mode: 'setup',
-				customer: 'cus_OvsQ7xyhnCsO8t',
-				success_url: `${process.env.URL}/user-menu`,
-				cancel_url: `${process.env.URL}/user-menu`,
-			})
-			redirect(session.url!)
-		} catch (error) {
-			throw error
-		}
-	}
+	// const setUpPaymentMethod = async (fd: FormData) => {
+	// 	'use server'
+	// 	try {
+	// 		//Save credit card info
+	// 		const session = await stripe.checkout.sessions.create({
+	// 			payment_method_types: ['card'],
+	// 			mode: 'setup',
+	// 			customer: 'cus_OvsQ7xyhnCsO8t',
+	// 			success_url: `${process.env.URL}/user-menu`,
+	// 			cancel_url: `${process.env.URL}/user-menu`,
+	// 		})
+	// 		redirect(session.url!)
+	// 	} catch (error) {
+	// 		throw error
+	// 	}
+	// }
 
 	return (
 		<div className='px-4 pt-8 leading-8 [&>h1]:text-2xl [&>h1]:py-4 [&>h1]:font-bold '>
-			<h1>My wallet</h1>
+			{/* <h1>My wallet</h1>
 
 			<form action={setUpPaymentMethod} >
 				<p>Selected Card: *{paymentMethods.data[0].card?.exp_year}</p>
@@ -79,7 +79,7 @@ export default async function Wallet() {
 
 			<form action={handleTopUp}>
 				<button>Test Charge</button>
-			</form>
+			</form> */}
 		</div>
 	)
 }
